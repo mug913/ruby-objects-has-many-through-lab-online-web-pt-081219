671f910a5f05eda09a6ad97745c08b3cc9dfa4b7
Brad Alexander <brad.alexander913@gmail.com>
2019-09-19 17:13:21 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointments 
+  
+  @@all = [] 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(date,patient,doctor)
+    @patient = patient
+    @doctor = doctor 
+    @date = date 
+    @all << self
+  end 
+  
+  
+  
+end 
