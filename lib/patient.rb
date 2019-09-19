3c8c9c921e918bcefff5b2929c58368ee3243362
Brad Alexander <brad.alexander913@gmail.com>
2019-09-19 17:11:49 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,31 @@
+class Patient 
+  @@all = [] 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  attr_accessor :name 
+  
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+  
+  def appointments 
+    Appointment.all.map do |appoint|
+      appoint.patient == self 
+    end 
+  end 
+  
+  def doctors
+    self.appointments.map do |appoint|
+      appoint.doctor
+    end 
+  end 
+  
+end 
